ab3429062886b27635fe1aedd28f9ff9d22450ff
bkerr <none@none>
2009-08-12 14:22:39 -0400

    Adjust settings_local_sample to better fit the (non-standard) layout of this project.

--- settings_local_sample.py
+++ settings_local_sample.py
@@ -1,14 +1,13 @@
 import os
 
-
-PROJECT_PATH = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__))))
+PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 LOCAL_DEVELOPMENT = True
 
 DATABASE_ENGINE = 'sqlite3'
-DATABASE_NAME = os.path.join(PROJECT_PATH, '..', '..', 'db', 'foia.sqlite')
+DATABASE_NAME = os.path.join(PROJECT_PATH, 'db', 'foia.sqlite')
 DATABASE_USER = ''
 DATABASE_PASSWORD = ''
 DATABASE_HOST = ''
